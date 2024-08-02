9fbf1fa5b73245c9abed6edf00086862603b84b8
thawfeek <mohamedthawfeek96@gmail.com>
2024-08-02 10:53:53 +0530

    new changes

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -114,6 +114,9 @@ const Dashboard = () => {
             />
           </div>
           <div className=" w-full h-[10%] flex items-center justify-between px-3 pt-4 relative 2xl:flex-row xl:flex-row lg:flex-row md:flex-row sm:flex-row xs:flex-col mobile:flex-col xss:flex-col gap-2">
+           {
+            tablebody?.length !== 0 && <>
+            
             <div className="w-[80px] z-30">
               <Select
                 options={limitCount}
@@ -134,6 +137,10 @@ const Dashboard = () => {
                 status={statusAvailable}
               />
             </div>
+            </>
+           }
+           
+           
           </div>
         </div>
       )}
