2774ed719274dca3b74a3ce097e6771ecd7e9453
thawfeek <mohamedthawfeek96@gmail.com>
2024-09-03 12:27:32 +0530

    new changes

--- src/utils/Validation/Login.jsx
+++ src/utils/Validation/Login.jsx
@@ -3,6 +3,6 @@ import * as yup from "yup";
 export const LoginSchema = yup.object().shape({
   phone_number: yup
     .string("Please enter your phone number")
-    .matches(/^[0-9]{9}$/, "Enter a valid phone number")
+    .matches(/^[0-9]{10}$/, "Enter a valid phone number")
     .required("Phone number is required"),
 });
